792d17768030b717d70f1e7a96b9ff6504a03cb7
Matthew <mtaylorex@gmail.com>
2023-02-27 14:28:32 -0500

    Move styles between containers

--- src/components/container.tsx
+++ src/components/container.tsx
@@ -7,9 +7,24 @@ interface Props {
 const Container: React.FC<Props> = (props) => (
   <>
   <TopBar />
-  <div className="mx-2 lg:mx-7 my-2 min-h-800 border-solid border-2 rounded-lg border-slate-400">
+  <div className="">
     {props.children}
   </div>
+
+  <hr className="w-64 px h-px mx-auto bg-slate-400 border-0 rounded mt-6 mb-4 md:mt-24 md:mb-4 dark:bg-gray-700"/>
+  <footer className="flex justify-center mx-2 lg:mx-7 mb-12">
+    <div className="flex-col">
+      <div className="font-mono text-center text-slate-500">
+        Built with 
+        <a className="text-sky-600 mx-2" href="https://www.typescriptlang.org/">TypeScript</a>
+        and 
+        <a className="text-sky-600 ml-2" href="https://nextjs.org/">Next.js</a>, 
+        deployed on 
+        <a className="text-sky-600 mx-2" href="https://pages.github.com/">GitHub Pages</a>
+      </div>
+      <div className="text-center text-blue-300 font-mono text-sm mt-2"><a href="https://github.com/m-ttaylor/matthew-personal-site">source</a></div>
+    </div>
+  </footer>
   </>
 )
 
--- src/components/pageBody.tsx
+++ src/components/pageBody.tsx
@@ -3,7 +3,7 @@ interface Props {
 }
 
 const PageBody: React.FC<Props> = (props) => (
-  <div className="w-full max-w-4xl p-1 my-5 lg:mx-auto">
+  <div className="w-full max-w-4xl my-2 px-2 py-2 mx-1 lg:mx-auto lg:max-w-4xl lg:px-6 border-solid border-2 rounded-lg border-slate-400">
     {props.children}
   </div>
 )
